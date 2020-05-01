368edac0cad0ae6870c6adfef30e9dfbafa48127
Nathaniel <64310904+Nathan-code-sudo@users.noreply.github.com>
2020-05-02 01:49:25 +0700

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int luckynumber[], {1, 2, 3, 4, 5, 6 ,7, 8, 9, 10,};
+                    //  0  1  2  3  4  5  6  7  8   9
+                                                        // This array counts from zero 
+    printf("%d", luckynumbers[3]);
     return 0;
 }
